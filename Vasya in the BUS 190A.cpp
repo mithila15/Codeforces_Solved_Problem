afac8cf372529eca3ed2ede527f05f56f86f2211
Mithila Akter Mim <112977859+mithila15@users.noreply.github.com>
2025-03-05 10:58:19 +0600

    Add files via upload

--- Vasya in the BUS 190A.cpp
+++ Vasya in the BUS 190A.cpp
@@ -1,51 +1,16 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <cstdio>
+
 int main()
 {
-        int n,m;
-        int max,min;
-
-        cin>>n>>m;
-
-        if(n==0 && m!=0)
-        {
-            cout<<"Impossible"<<endl;
-            return 0;
-        }
-
-        else if(n==0 && m==0)
-        {
-            cout<<"0 "<<"0"<<endl;
-        }
-
-        else if(m==0 && n!=0)
-        {
-            cout<<n<<" "<<n<<endl;
-            return 0;
-        }
-
-        else
-        {
-             if(m>n)
-             {
-                 max=m+n-1;
-                 min=(n-1)+(m-n)+1;
-             }
-
-             else if(m==n)
-             {
-                 max=m+n-1;
-                 min=m;
-             }
-
-             else
-             {
-                 max=m+n-1;
-                 min=m+n-m;
-             }
-
-             cout<<min<<" "<<max<<endl;
-        }
-
-        return 0;
+    int n, m;
+    scanf("%d%d", &n, &m);
+    if (n != 0 || m == 0)
+    {
+        printf("%d %d\n", (m > n ? m : n), (m != 0 ? n + m - 1 : n));
+    }
+    else
+    {
+        printf("Impossible\n");
+    }
+    return 0;
 }
