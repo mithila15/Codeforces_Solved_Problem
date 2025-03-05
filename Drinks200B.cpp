893ea0b17e5aa065cc73169f9e33b48e83cf49f1
Mithila Akter Mim <112977859+mithila15@users.noreply.github.com>
2025-03-05 11:00:53 +0600

    Add files via upload

---
+++ Drinks200B.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, p, total(0);
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> p;
+        total += p;
+    }
+    cout << static_cast<double>(total) / static_cast<double>(n) << endl;
+    return 0;
+}
