e467aaf278ee1ef26111277eaa660084a9807d45
Mithila Akter Mim <112977859+mithila15@users.noreply.github.com>
2025-03-09 02:25:59 +0600

    Add files via upload

---
+++ Prizes208D.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+
+int main()
+{
+    int n, p[50], a, b, c, d, e, points(0), mug(0), towel(0), bag(0), bicycle(0), car(0);
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i)
+    {
+        scanf("%d", &p[i]);
+    }
+    scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
+
+    for (int i = 0; i < n; ++i)
+    {
+        points += p[i];
+
+        car += points / e;
+        points %= e;
+        bicycle += points / d;
+        points %= d;
+        bag += points / c;
+        points %= c;
+        towel += points / b;
+        points %= b;
+        mug += points / a;
+        points %= a;
+    }
+    printf("%I64d %I64d %I64d %I64d %I64d\n%d\n", mug, towel, bag, bicycle, car, points);
+    return 0;
+}
