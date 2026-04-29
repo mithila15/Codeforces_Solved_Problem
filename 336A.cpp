094682dafdc42c34747a30a489932ad74dfef7ab
Mithila Akter Mim <112977859+mithila15@users.noreply.github.com>
2026-04-29 11:30:06 +0600

    Add files via upload

---
+++ 336A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    if (x > 0)
+    {
+        if (y > 0)
+        {
+            cout << 0 << " " << x + y << " " << x + y << " " << 0 << endl;
+        }
+        else
+        {
+            cout << 0 << " " << y - x << " " << x - y << " " << 0 << endl;;
+        }
+    }
+    else
+    {
+        if (y > 0)
+        {
+            cout << x - y << " " << 0 << " " << 0 << " " << y - x << endl;;
+        }
+        else
+        {
+            cout << x + y << " " << 0 << " " << 0 << " " << x + y << endl;;
+        }
+    }
+    return 0;
+}
