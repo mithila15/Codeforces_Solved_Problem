135e69005613dfb87786b397688c40aa5f8d60da
Mithila Akter Mim <112977859+mithila15@users.noreply.github.com>
2026-05-03 01:16:07 +0600

    Add files via upload

---
+++ 333B.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+int main()
+{
+    int n, m, x, y, row[1001], col[1001];
+    scanf("%d%d", &n, &m);
+    fill(row + 1, row + n + 1, 1);
+    fill(col + 1, col + n + 1, 1);
+
+    while (m--)
+    {
+        scanf("%d%d", &x, &y);
+        row[x] = 0;
+        col[y] = 0;
+    }
+
+    int points = accumulate(row + 2, row + n, 0) + accumulate(col + 2, col + n, 0);
+    if (n >= 3 && n % 2 == 1 && row[n / 2 + 1] == 1 && col[n / 2 + 1] == 1)
+    {
+        points -= 1;
+    }
+
+    printf("%d\n", points);
+    return 0;
+}
