7d96375fd76628b6146cb1aa8288e865686b3399
Mithila Akter Mim <112977859+mithila15@users.noreply.github.com>
2026-05-06 23:49:23 +0600

    Add files via upload

---
+++ 322.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    unsigned r, g, b;
+    cin >> r >> g >> b;
+
+    unsigned R(r % 3), G(g % 3), B(b % 3);
+    unsigned amount(r / 3 + g / 3 + b / 3 + min(R, min(G, B)));
+    if (   R == 2 && G == 2 && B == 0 && b != 0
+        || G == 2 && B == 2 && R == 0 && r != 0
+        || B == 2 && R == 2 && G == 0 && g != 0)
+    {
+        amount += 1;
+    }
+    cout << amount << endl;
+    return 0;
+}
