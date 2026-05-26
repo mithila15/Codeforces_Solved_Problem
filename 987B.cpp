5c025e690979f302d07db4c6a0c1f6d6d31bce59
Mithila Akter Mim <112977859+mithila15@users.noreply.github.com>
2026-05-26 15:04:53 +0600

    Add files via upload

---
+++ 987B.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+long long int x,y;
+long double X,Y;
+int main()
+{
+    fastread();
+    cin>>x>>y;
+    X=y*log(x);
+    Y=x*log(y);
+
+//    cout<<X<<" "<<Y<<"\n";
+    if(X==Y){
+        cout<<"=\n";
+    }
+    else if(X>Y){
+        cout<<">\n";
+    }
+    else if(X<Y){
+        cout<<"<\n";
+    }
+
+    return 0;
+}
