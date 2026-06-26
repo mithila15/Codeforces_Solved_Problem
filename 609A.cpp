65f028625f4420498b4413e372b60a48fced20ae
Mithila Akter Mim <112977859+mithila15@users.noreply.github.com>
2026-06-26 10:35:53 +0600

    Add files via upload

---
+++ 609A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,a[101];
+    long long int m,sum=0;
+    cin>>n>>m;
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    sort(a,a+n,greater<int>());
+
+    int c=0;
+    for(int i=0; i<n; i++){
+        c++;
+        sum+=a[i];
+        if(sum>=m){
+            break;
+        }
+    }
+    cout<<c<<endl;
+
+    return 0;
+}
+
